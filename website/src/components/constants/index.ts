6e8292e18988b918562564beb7bb5fe1bddf84e4
Davidxing1 <937104001@qq.com>
2023-02-15 19:05:30 +0800

    update web

--- website/src/components/constants/index.ts
+++ website/src/components/constants/index.ts
@@ -1,8 +1,6 @@
-const packageObjectId = '0xca9d65e59b8d1ff475b96c8af5dc41fa623e4afe'
-const marketplaceObjectId = '0x4aaf15bc0f510a57d683d66df23c1277f6d88020'
+const packageObjectId = '0x40807ef32dd990cf1ba801dc63824ba8a80684da'
+const marketplaceObjectId = '0x0e355524cc51c523187d13e6507085a41157f907'
 const gamePackageObjectId = '0x21872eab1d01d73bc1bddc38a1e761780281ff08'
 export {
-    marketplaceObjectId,
-    packageObjectId,
-    gamePackageObjectId
+    marketplaceObjectId,packageObjectId,gamePackageObjectId
 }
