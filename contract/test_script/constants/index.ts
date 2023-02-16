577d155d2345b49b64c53cf897fa4f807a0cd61a
henryliu <17600047539@163.com>
2023-02-16 12:47:50 +0800

    update

--- contract/test_script/constants/index.ts
+++ contract/test_script/constants/index.ts
@@ -1,5 +1,5 @@
-const packageObjectId = '0x00ca00095635ce33c9a97091ade1a110575431e9'
-const marketplaceObjectId = '0x2ba4661d0a835a686c7c2a1c5d22279769c40d44'
+const packageObjectId = '0x4b5040ba857d181b13ecc10cf5f60bd3c2b523c1'
+const marketplaceObjectId = '0x8e75d5076fe4bedaa65c28d3606df4ff0a3eb1d2'
 const gamePackageObjectId = '0xd617b5a28ea3a855979799ebbb00e311b4dbe226'
 export {
     marketplaceObjectId,packageObjectId,gamePackageObjectId
